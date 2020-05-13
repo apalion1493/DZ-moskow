461107e81c045159a09e1936843cd4d25fd8983e
apalion1493 <39518411+apalion1493@users.noreply.github.com>
2020-05-13 16:51:58 +0300

    commit 13.05 v3

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,16 +8,26 @@ burger.addEventListener('click', function () {
 
 var controller = new ScrollMagic.Controller();
 
-var tween1 = TweenMax.to("#attraction__img-plane", 0.5, {top: '-300px', transform: 'rotate(15deg)', scale: 1});
-var tween2 = TweenMax.to("#attraction__img-humans", 0.5, {top: '100px', transform: 'rotate(-5deg)', scale: 1});
+var tween1 = TweenMax.to("#attraction__img-plane", 0.5, {top: '-350px', transform: 'rotate(15deg)', scale: 1});
+var tween2 = TweenMax.to("#attraction__img-humans", 0.5, {top: '180px', transform: 'rotate(-5deg)', scale: 1.2});
 
-var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 500, offset: 0})
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 800, offset: 0})
     .setTween(tween1)
     // .setTween("#attraction__img-plane", {scale: 0.7})
-    // .addIndicators()
+    //.addIndicators()
     .addTo(controller);
 
-var scene2 = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 500, offset: 0})
+var scene2 = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 800, offset: 0})
     .setTween(tween2)
-    // .addIndicators()
+    //.addIndicators()
     .addTo(controller);
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 110) {
+        $('.header').addClass('fixed');
+        $('.tandem-first-screen').addClass('active');
+    } else {
+        $('.header').removeClass('fixed');
+        $('.tandem-first-screen').removeClass('active')
+    }
+});
